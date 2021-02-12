9bcac9390c06f11cc26e2fa1c24902c40705b7ba
ShashankKNaik <76618871+ShashankKNaik@users.noreply.github.com>
2021-02-12 10:01:47 +0530

    Create decisionTree.py

---
+++ Machine Learning/decisionTree.py
@@ -0,0 +1,28 @@
+"""
+Created on Thu Feb 11 18:45:40 2021
+
+@author: shashank
+"""
+
+#Loan approval classification based on applicants status and previous data
+
+from sklearn import tree
+import numpy as np
+from matplotlib import pyplot as plt
+
+#status=["age{y=0/m=1/o=2}","has_job{0/1}","own_house{0/1}","credit_rating{f=0/g=1/e=2}"]
+
+status=np.array([[0,0,0,0],[0,0,0,1],[0,1,0,1],[0,1,1,0],[0,0,0,0],
+                 [1,0,0,0],[1,0,0,1],[1,1,1,1],[1,0,1,2],[1,0,1,2],
+                 [2,0,1,2],[2,0,1,1],[2,1,0,1],[2,1,0,2],[2,0,0,0]])
+
+#class{approved=1/not=0}
+
+clas=np.array([0,0,1,1,0,0,0,1,1,1,1,1,1,1,0])
+
+classifier=tree.DecisionTreeClassifier().fit(status,clas)
+
+print (classifier.predict([[0,0,1,0]]))
+
+fig = plt.figure(figsize=(50,40))
+_ = tree.plot_tree(classifier)
